363293aeb36d76fe201cd10041b53c515e2ac908
gjustin40 <gjustin@naver.com>
2021-03-07 19:32:30 +0900

    Solved

---
+++ baekjoon/baekjoon1436.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+count = 665
+rank = 0
+while rank != N:
+    count += 1
+    if '666' in str(count):
+        rank += 1
+    
+print(count)
---
+++ baekjoon/baekjoon2750.py
@@ -0,0 +1,6 @@
+num_list = []
+for _ in range(int(input())):
+    num_list.append(int(input()))
+
+for i in sorted(num_list):
+    print(i)
