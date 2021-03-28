0e72350c30e8e6b943ae92c07bf543ffb95cdf19
gjustin40 <gjustin@naver.com>
2021-03-28 19:40:15 +0900

    Solved

--- baekjoon/baekjoon1931_retry.py
+++ baekjoon/baekjoon1931_retry.py
@@ -4,8 +4,8 @@ N = int(input())
 time_list = []
 
 for _ in range(N):
-    a, b = map(int, sys.stdin.readline().split())
-    time_list.append([a, b])
+    a, b = map(int, sys.stdin.readline().split()) # 그냥 input()보다 sys.stdin.readline()으로 하니까 매우 빠름
+    time_list.append([a, b])                      # 추가로 jupyter notebook에서는 sys가 잘 작동하지 않음......
     
 time_list = sorted(time_list, key=lambda x : x[0])
 time_list = sorted(time_list, key=lambda x : x[1])
