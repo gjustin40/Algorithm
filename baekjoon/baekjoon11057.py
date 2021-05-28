1f12e797674a13cd3aa6a81bfd66d96fb9f89145
gjustin <gjustin@naver.com>
2021-05-28 23:17:26 +0900

    Solved

---
+++ baekjoon/baekjoon11057.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+dp = [[0]*10 for _ in range(1001)]
+
+dp[1] = [1] * 10
+
+for i in range(2, N+1):
+    for j in range(10):
+        dp[i][j] = sum(dp[i-1][:j+1])
+
+print(sum(dp[N]) % 10007)
